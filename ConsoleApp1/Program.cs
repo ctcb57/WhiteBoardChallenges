7dd991941c4c57ccd699d906cc9d61dc68c2f202
Charles Clark <ctcb57@gmail.com>
2019-10-09 22:59:52 -0500

    Completed challenge which forces user to remove duplicates from a given array

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp1.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sandbox box = new Sandbox();
+            int []A = { 4, 5, 7, 8, 4, 6 };
+            int [] result = box.ReturnNoDuplicates(A);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Properties/Sandbox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Properties
+{
+    class Sandbox
+    {
+        public int [] A;
+
+
+
+        public int AddDoubleDigits(int[] A)
+        {
+            int result = 0;
+            for(int i = 0; i < A.Count(); i++)
+            {
+                if(A[i] > 9 && A[i] < 100)
+                {
+                    result += A[i];
+                }
+            }
+            return result;
+        }
+        // Create method which removes duplicates from an array
+        public int [] ReturnNoDuplicates(int [] test)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < test.Length; i++)
+            {
+                for(int j = 1; j < test.Length; j++)
+                {
+                    if(test[i] == test[j])
+                    {
+                        if (result.Contains(test[i]))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            result.Add(test[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+            int[] actualResult = result.ToArray();
+            return actualResult;
+        }
+    }
+}
